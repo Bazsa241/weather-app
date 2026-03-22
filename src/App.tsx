884e60c86ce8ac31e882d0dea76b3a677427e2ff
Balazs Horvath <balazs.horvath@zenik-solutions.com>
2026-03-22 16:50:15 +0100

    feat: add AppLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { Typography } from '@mui/material';
-import { Header } from '@app/components';
+import { AppLayout } from '@app/layouts';
 
 export const App = () => {
   return (
-    <>
-      <Header />
-      <div className="flex justify-center">
-        <Typography variant="h3" component="h1">
-          Weather App
-        </Typography>
-      </div>
-    </>
+    <AppLayout>
+      <Typography variant="h3" component="h1" textAlign="center">
+        Weather App
+      </Typography>
+    </AppLayout>
   );
 };
---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,22 @@
+import { Header } from '@app/components';
+import { useContext } from 'react';
+import { ColorModeContext } from '@app/theme';
+
+export const AppLayout = ({ children }: { children: React.ReactNode }) => {
+  const { colorMode } = useContext(ColorModeContext);
+
+  return (
+    <div
+      className={`flex flex-col h-screen ${
+        colorMode === 'light'
+          ? 'bg-linear-to-b from-blue-100 to-blue-500'
+          : 'bg-linear-to-b from-blue-950 to-blue-500'
+      }`}
+    >
+      <Header />
+      <main className="flex-1 overflow-y-auto p-4">
+        <div className="max-w-5xl mx-auto flex flex-col gap-6">{children}</div>
+      </main>
+    </div>
+  );
+};
