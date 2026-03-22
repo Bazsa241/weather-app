5a0897fafb4b7f97d14569da069420b4824dafdf
Balazs Horvath <balazs.horvath@zenik-solutions.com>
2026-03-22 16:51:33 +0100

    refactor: update header layout and remove AppBar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,14 @@
-import { AppBar, Toolbar } from '@mui/material';
 import { ThemeSwitcher } from './ThemeSwitcher';
 import { LanguageSwitcher } from './LanguageSwitcher';
 import { HeaderContentDivider } from './HeaderContentDivider';
+import { Box } from '@mui/material';
 
 export const Header = () => {
   return (
-    <AppBar position="static">
-      <Toolbar className="flex justify-end gap-2">
-        <LanguageSwitcher />
-        <HeaderContentDivider />
-        <ThemeSwitcher />
-      </Toolbar>
-    </AppBar>
+    <Box className="flex justify-end gap-2 px-6 py-2 min-h-16">
+      <LanguageSwitcher />
+      <HeaderContentDivider />
+      <ThemeSwitcher />
+    </Box>
   );
 };
